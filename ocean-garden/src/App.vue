1e38b4de653cb4e3b49b10fb155a38a33a7a68cb
Milton Garcia <mgarcia@comextweb.com>
2023-10-07 10:30:22 -0500

    feat: Mapa base en vista main

--- ocean-garden/src/App.vue
+++ ocean-garden/src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-app>
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+  <v-app >
+<!--    <img alt="Vue logo" src="./assets/images/logo.png">-->
 <!--    <HelloWorld msg="Ganamos ir a la NASA Vue.js App"/>-->
-      <FitoplanctonCards />
+<!--      <FitoplanctonCards />-->
+      <MainLayout />
   </v-app>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import FitoplanctonCards from "@/components/FitoplanctonCards.vue";
+import MainLayout from "@/components/MainLayout.vue";
 
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    FitoplanctonCards
+    // FitoplanctonCards
+    MainLayout
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
 </style>
---
+++ ocean-garden/src/components/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container-game-map">
+        <img src="@/assets/images/super-mario-world-map.jpg" class="game-map">
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    /* Reset default margin and padding */
+    body, html {
+        margin: 0;
+        padding: 0;
+        overflow: hidden; /* Hide vertical scrollbar */
+    }
+
+    /* Set full width and height for the container */
+    .container-game-map {
+        width: 100vw!important; /* 100% of viewport width */
+        height: 100vh!important; /* 100% of viewport height */
+        overflow-x: auto!important; /* Enable horizontal scrolling */
+        overflow-y: auto!important;
+        white-space: nowrap!important; /* Prevent text wrapping */
+    }
+
+    /* Set the image to cover the entire container */
+    .game-map {
+        width: 100%!important; /* Allow the image to have its natural width */
+        height: 100%!important; /* 100% height of container */
+        verflow-y: hidden!important;
+        object-fit: cover!important; /* Ensure the image covers the container */
+        transform: translateX(0)!important; /* Initialize horizontal position */
+        animation: scrollImage 60s linear infinite!important; /* Horizontal scrolling animation */
+    }
+
+    /*@media (max-width: 600px) {*/
+    /*     .game-map {*/
+    /*        width: 100%!important; !* Cover full width while maintaining aspect ratio *!*/
+    /*        height: 100%!important;*/
+    /*        overflow-y: hidden!important;*/
+    /*    }*/
+    /*}*/
+</style>
